df3de0e41618e096497d3f5883cc935708d42d06
Adi Meirovitch <adime.support@gmail.com>
2017-11-30 13:22:50 +0200

    adding printing with the calc burtton

--- app/main/mainCtrl.js
+++ app/main/mainCtrl.js
@@ -1,7 +1,13 @@
 GloCalc.controller("MainCtrl", function ($scope, Dosege, $log) {
     $scope.test = "ewewew";
+    $scope.InsulinUnits = "";
 
-    $scope.dosegeUser = new Dosege(10, 20);
    
-    $log.log($scope.dosegeUser);
+  
+    $scope.showDosege=function(){
+        $scope.dosegeUser = new Dosege($scope.Insulin, $scope.carbohydrate);
+       $scope.InsulinUnits = $scope.dosegeUser.calc() + "units";
+    }
+   
+   
 });
--- app/main/mainSrv.js
+++ app/main/mainSrv.js
@@ -8,26 +8,8 @@ GloCalc.factory("Dosege", function() {
             var doseResult = this.carbohydrate + this.insulin;
             return doseResult;
         }
-        /*        this.kmPerYear = function() {
-            var currentYear = new Date().getFullYear();
-            return Math.round(this.km / (currentYear - this.year + 1));
-        };
-        */
+      
     }
 
     return Dosege;
 });
-
-//var dosegeUser = new Dosege(10,20);
-
-
-//   function setInsulin(carsPlain) {
-//     u = [];
-//     for (var i = 0; i < carsPlain.length; i++) {
-//         cars.push(new Car(carsPlain[i].brand, carsPlain[i].model, 
-//             carsPlain[i].year, carsPlain[i].km))
-//     }
-// }
-// function dosege (params) {
-//     users=[];
-//   users.push
