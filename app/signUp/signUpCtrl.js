9765085e291e9fd256d4163f5c0289077a3b34d0
Adi Meirovitch <adime.support@gmail.com>
2017-12-05 20:59:45 +0200

    changing Name to name (signUp) moving the activeUser.login & $location.path inside the createUser

--- app/signUp/signUpCtrl.js
+++ app/signUp/signUpCtrl.js
@@ -4,14 +4,18 @@ GloCalc.controller("signUpCtrl", function ($log, $scope, $http, $location, activ
 
     $scope.createUser = function () {
        $scope.user = new User($scope.name,$scope.address,$scope.email,$scope.pwd,$scope.radioSex,$scope.diabetesType,$scope.ratioCarbon,$scope.ratioCorrect);
+     
+       if ($scope.user !=null) {
+        activeUser.login($scope.user);
+        $location.path("/main");
+    }
+     
     };
 
-    activeUser.login($scope.user);
+    
 
  //   $scope.users.push($scope.user);
 
-    $location.path("/main");
-
-   
+    
 
 });
