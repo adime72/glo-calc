5f562d9da24a41b0237c448d366d069e0ba9298e
Adi Meirovitch <adime.support@gmail.com>
2017-12-06 02:29:12 +0200

    fixing the nav bar in the index (collapse - hamburger) trying to understand why we dont get the ratio parms to main

--- app/signUp/signUpCtrl.js
+++ app/signUp/signUpCtrl.js
@@ -1,7 +1,9 @@
 GloCalc.controller("signUpCtrl", function ($log, $scope, $http, $location, activeUser, User) {
     // 4 12 2017 
     //console.log(user);
-
+    //==============================================================
+    //the function do not get the parametrsfrom the active user.
+    //===============================================================
     $scope.createUser = function () {
        $scope.user = new User($scope.name,$scope.address,$scope.email,$scope.pwd,$scope.radioSex,$scope.diabetesType,$scope.ratioCarbon,$scope.ratioCorrect);
      
